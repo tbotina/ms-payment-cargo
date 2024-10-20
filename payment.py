ffa30566780b1d700c228aa9c2a96dd4fabffc8f
tbotina <tania.botina22736@ucaldas.edu.co>
2024-10-20 12:08:07 -0500

    Implementación pasarela de pagos, corregir el error en CC

--- payment.py
+++ payment.py
@@ -1,51 +1,103 @@
-import requests
+import json 
+from dotenv import load_dotenv
+from epaycosdk.epayco import Epayco
+from flask import Flask, request, jsonify
+import os
 
-# Datos de autenticación y URL de la API de ePayco
-url = "https://secure.epayco.co/validation/v1/reference"
-headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer <private_key>'  # Reemplazar con tu private_key
-}
+# Cargar variables de entorno
+load_dotenv()
 
-# Datos de la solicitud de pago
-data = {
-    "public_key": "<public_key>",  # Reemplazar con tu public_key
-    "card_number": "4575623182290326",  # Número de tarjeta de prueba de ePayco
-    "card_exp_year": "22",  # Año de expiración (dos dígitos)
-    "card_exp_month": "12",  # Mes de expiración
-    "card_cvc": "123",  # Código de seguridad de la tarjeta
-    "doc_type": "CC",  # Tipo de documento (CC: cédula de ciudadanía, TI: tarjeta de identidad)
-    "doc_number": "123456789",  # Número de documento del pagador
-    "name": "Juan Perez",  # Nombre del tarjetahabiente
-    "last_name": "Lopez",  # Apellido del tarjetahabiente
-    "email": "juanperez@gmail.com",  # Correo electrónico del pagador
-    "invoice": "1234",  # Número de factura o referencia
-    "description": "Pago de prueba",  # Descripción del pago
-    "value": "10000",  # Valor del pago
-    "tax": "1600",  # Impuesto si aplica
-    "tax_base": "8400",  # Base imponible
-    "currency": "COP",  # Moneda
-    "dues": "1",  # Número de cuotas
-}
+app = Flask(__name__)
+epayco = Epayco({
+    "apiKey": os.getenv("EPAYCO_PUBLIC_KEY"),
+    "privateKey": os.getenv("EPAYCO_PRIVATE_KEY"),
+    "lenguage": "ES",
+    "test": os.getenv("EPAYCO_TEST") == "True" 
+})
 
-# Función para realizar el pago
-def realizar_pago():
+def create_token(data):
+    print(data)
     try:
-        # Realizar solicitud POST a la API de ePayco
-        response = requests.post(url, json=data, headers=headers)
-        response_data = response.json()
-
-        # Verificar si el pago fue exitoso
-        if response_data['status'] == 'success':
-            token = response_data['data']['transactionID']
-            print(f"Pago realizado con éxito. Token de confirmación: {token}")
-            return token
-        else:
-            print(f"Error en el pago: {response_data['message']}")
-            return None
+        print("Entro al create_token")
+        card_info = {
+            "card[number]": data["card_number"],
+            "card[exp_year]": data["exp_year"],
+            "card[exp_month]": data["exp_month"],
+            "card[cvc]": data["cvc"],
+            "hasCvv": True
+        }
+        token= epayco.token.create(card_info)
+        print("Token generado", token)
+        return token
     except Exception as e:
-        print(f"Error al realizar el pago: {e}")
-        return None
+        return {"error al crear token": str(e)}
 
-# Ejecutar la función
-realizar_pago()
+def create_customer(token, data):
+    customer_info = {
+        "name": data["name"],
+        "last_name": data["last_name"],
+        "email": data["email"],
+        "phone": data["phone"],
+        "default": True
+    }
+    customer_info["token_card"] = token
+    try:
+        customer = epayco.customer.create(customer_info)
+        return customer
+    except Exception as e:
+        return {"error al crear el usuario": str(e)}
+
+def process_payment(data, customer_id, token_card):
+    try:
+        payment_info = {
+            "token_card": token_card,
+            "customer_id": customer_id,
+            "doc_type": "CC",
+            "doc_number": data["doc_number"],
+            "name": data["name"],
+            "last_name": data["last_name"],
+            "email": data["email"],
+            "city": data["city"],
+            "address": data["address"],
+            "phone": data["phone"],
+            "cell_phone": data["cell_phone"],
+            "bill": data["bill"],
+            "description": "Pago servicio de transporte",
+            "value": data["value"],
+            "tax": 0,
+            "tax_base": data["value"],
+            "currency": "COP"
+        }
+        response= epayco.charge.create(payment_info)
+        return response
+    except Exception as e:
+        return {"error al procesar el pago": str(e)}
+    
+@app.route("/process-payment", methods=["POST"])
+def handle_process_payment():
+    data = request.json
+    token_response = create_token(data)
+    print("Token response", json.dumps(token_response))
+
+    if token_response["status"] is False:
+        return jsonify(token_response), 500
+    token_card = token_response["id"]
+
+    customer_response = create_customer(token_card, data)
+    print("Customer response", json.dumps(customer_response))
+
+    if "error" in customer_response:
+        return jsonify(customer_response), 500
+
+    customer_id = customer_response["data"]["customerId"]
+
+    payment_response = process_payment(data, customer_id, token_card)
+    print("Payment response", json.dumps(payment_response))
+
+    if "error" in payment_response:
+        return jsonify(payment_response), 500
+    
+    return jsonify(payment_response), 200
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
